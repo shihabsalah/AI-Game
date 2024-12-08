588e0796b8b717cd12ba4cbf6b6ab962758c35bf
Shihab Salah <shihabsalahramadan@gmail.com>
2024-12-08 19:18:32 +0200

    Moving UI

---
+++ View/UI/MazeView.py
@@ -0,0 +1,11 @@
+# MazeView.py
+import pygame
+from CONSTANTS import *
+
+class MazeView:
+    def __init__(self, maze_model):
+        self.maze_model = maze_model
+
+    def draw(self, screen, player_pos, exit_pos, ai_path):
+        # Implement drawing logic using self.maze_model
+        pass
